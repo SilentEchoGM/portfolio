c3b2b2c427ca856add834716622fb31c12cf15ab
Silent Echo <silent@silentecho.eu>
2024-03-28 00:12:54 +0100

    initial commit after rebuilding on current sveltekit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<h1>Silent Echo</h1>
+<h2>TS, Svelte, C# Developer</h2>
+
+<br />
+<h3>Articles</h3>
+<ul>
+  <li>
+    <a href="./articles/basic-sos">Getting Started with Svelte and SOS</a>
+  </li>
+</ul>
+
+<br />
+<h3>Tools</h3>
+<ul>
+  <li>
+    <a
+      href="https://marketplace.visualstudio.com/items?itemName=silentechogm.fpts-import-helper"
+      >fp-ts Import Helper VSCode Extension</a>
+  </li>
+  <!-- <li>
+    <a href="./tools/marvel-damage">Marvel Multiverse Damage Calculator</a>
+  </li> -->
+</ul>
